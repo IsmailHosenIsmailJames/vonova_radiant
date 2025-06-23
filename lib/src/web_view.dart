2d9d7208bf6d490f2e59a00bb3f71d1b643b1dca
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2025-06-23 15:13:59 +0600

    Refactor: Enhance WebView functionality and styling
    - Refactored `WebViewInAppState` in `lib/src/web_view.dart`:
        - Renamed variables for clarity (e.g., `webViewKey` to `_webViewKey`).
        - Introduced helper methods for initialization (e.g., `_initializeContextMenu`, `_initializePullToRefreshController`, `_initializeWebView`).
        - Added `_initialUrl` constant.
        - Modified `initialUserScripts` to inject custom CSS at document start and end.
        - Improved error handling in `_handleLoadError` with separate navigation for internet and page availability issues.
        - Streamlined reload logic in `_attemptReload`.
        - Updated PopScope behavior.
        - Removed unused `deleteFolder` method.
        - Removed unused `dart:io` import.
    - Added `lib/src/app_css.dart`:
        - Defines `appCSS` string containing custom CSS for the web content.
        - Includes font definitions, color variables for light and dark themes, and styles for toast notifications and modals.

--- lib/src/web_view.dart
+++ lib/src/web_view.dart
@@ -1,11 +1,11 @@
 import 'dart:collection';
-import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:vonova_radiant/src/app_css.dart';
 import 'package:vonova_radiant/src/do_data_found.dart';
 import 'package:vonova_radiant/src/no_internet.dart';
 
@@ -19,10 +19,12 @@ class WebViewInApp extends StatefulWidget {
 }
 
 class WebViewInAppState extends State<WebViewInApp> {
-  final GlobalKey webViewKey = GlobalKey();
+  final GlobalKey _webViewKey = GlobalKey();
+  InAppWebViewController? _webViewController;
 
-  InAppWebViewController? webViewController;
-  InAppWebViewSettings settings = InAppWebViewSettings(
+  static const String _initialUrl = "http://103.168.140.134:5004/";
+
+  final InAppWebViewSettings _webViewSettings = InAppWebViewSettings(
     isInspectable: kDebugMode,
     cacheMode: CacheMode.LOAD_CACHE_ELSE_NETWORK,
     mediaPlaybackRequiresUserGesture: false,
@@ -31,52 +33,129 @@ class WebViewInAppState extends State<WebViewInApp> {
     iframeAllowFullscreen: true,
   );
 
-  PullToRefreshController? pullToRefreshController;
+  PullToRefreshController? _pullToRefreshController;
+  late ContextMenu _contextMenu;
+  double _progress = 0;
+  Widget _webViewWidget = const Center(
+    child: CircularProgressIndicator(),
+  ); // Renamed and initialized
 
-  late ContextMenu contextMenu;
-  double progress = 0;
-  Widget initWidget = const Center(child: CircularProgressIndicator());
+  @override
+  void initState() {
+    super.initState();
+    inAppUpdateAndroid(
+      context,
+    ); // Consider if this needs to be awaited or handled differently
+    _initializeContextMenu();
+    _initializePullToRefreshController();
+    _initializeWebView();
+  }
 
-  void initLastWebUrl() async {
-    String initUrl = "http://103.168.140.134:5004/";
-    setState(() {
-      initWidget = InAppWebView(
-        key: webViewKey,
-        // initialFile: initUrl,
-        initialUrlRequest: URLRequest(url: WebUri(initUrl)),
-        // initialUrlRequest:
-        // URLRequest(url: WebUri(Uri.base.toString().replaceFirst("/#/", "/") + 'page.html')),
-        // initialFile: "assets/index.html",
-        initialUserScripts: UnmodifiableListView<UserScript>([]),
-        initialSettings: settings,
-        contextMenu: contextMenu,
-        pullToRefreshController: pullToRefreshController,
-        onWebViewCreated: (controller) async {
-          webViewController = controller;
-        },
-        onLoadStart: (controller, url) async {},
+  void _initializeContextMenu() {
+    _contextMenu = ContextMenu(
+      menuItems: [
+        ContextMenuItem(
+          id: 1,
+          title:
+              "Special", // Consider a more descriptive title or make it a constant
+          action: () async {
+            await _webViewController?.clearFocus();
+          },
+        ),
+      ],
+      settings: ContextMenuSettings(hideDefaultSystemContextMenuItems: false),
+      onCreateContextMenu: (hitTestResult) async {
+        // Handle context menu creation if needed
+      },
+    );
+  }
 
-        onPermissionRequest: (controller, request) async {
-          return PermissionResponse(
-            resources: request.resources,
-            action: PermissionResponseAction.GRANT,
-          );
-        },
-
-        onReceivedError: (controller, request, error) {
-          pullToRefreshController?.endRefreshing();
-          _handleLoadError();
-        },
-        onProgressChanged: (controller, progress) {
-          if (progress == 100) {
-            pullToRefreshController?.endRefreshing();
-          }
-          setState(() {
-            this.progress = progress / 100;
-          });
-        },
+  void _initializePullToRefreshController() {
+    if (kIsWeb ||
+        ![
+          TargetPlatform.iOS,
+          TargetPlatform.android,
+        ].contains(defaultTargetPlatform)) {
+      _pullToRefreshController = null;
+    } else {
+      _pullToRefreshController = PullToRefreshController(
+        settings: PullToRefreshSettings(color: Colors.blue),
+        onRefresh: _attemptReload,
       );
+    }
+  }
+
+  void _initializeWebView() {
+    setState(() {
+      _webViewWidget = _buildInAppWebView();
     });
+  }
+
+  Widget _buildInAppWebView() {
+    return InAppWebView(
+      key: _webViewKey,
+      initialUrlRequest: URLRequest(url: WebUri(_initialUrl)),
+      initialSettings: _webViewSettings,
+      contextMenu: _contextMenu,
+      pullToRefreshController: _pullToRefreshController,
+      onWebViewCreated: (controller) {
+        _webViewController = controller;
+      },
+      initialUserScripts: UnmodifiableListView<UserScript>([
+        UserScript(
+          source:
+              """
+      var style = document.createElement('style');
+      style.id = 'flutter-injected-style';
+      style.innerHTML = `$appCSS`;
+      
+      // Remove the old style if it already exists
+      var oldStyle = document.getElementById('flutter-injected-style');
+      if (oldStyle) {
+        oldStyle.remove();
+      }
+      
+      document.head.appendChild(style);
+    """,
+          injectionTime: UserScriptInjectionTime.AT_DOCUMENT_START,
+        ),
+        UserScript(
+          source:
+          """
+      var style = document.createElement('style');
+      style.id = 'flutter-injected-style';
+      style.innerHTML = `$appCSS`;
+      
+      // Remove the old style if it already exists
+      var oldStyle = document.getElementById('flutter-injected-style');
+      if (oldStyle) {
+        oldStyle.remove();
+      }
+      
+      document.head.appendChild(style);
+    """,
+          injectionTime: UserScriptInjectionTime.AT_DOCUMENT_END,
+        ),
+      ]),
+      onPermissionRequest: (controller, request) async {
+        return PermissionResponse(
+          resources: request.resources,
+          action: PermissionResponseAction.GRANT,
+        );
+      },
+      onReceivedError: (controller, request, error) {
+        _pullToRefreshController?.endRefreshing();
+        _handleLoadError();
+      },
+      onProgressChanged: (controller, progress) {
+        if (progress == 100) {
+          _pullToRefreshController?.endRefreshing();
+        }
+        setState(() {
+          _progress = progress / 100;
+        });
+      },
+    );
   }
 
   Future<bool> _isInternetAvailable() async {
@@ -84,82 +163,42 @@ class WebViewInAppState extends State<WebViewInApp> {
         InternetStatus.connected;
   }
 
-  void _handleLoadError() async {
-    bool hasInternet = await _isInternetAvailable();
-    if (!hasInternet) {
-      // No internet, navigate to error page
+  Future<void> _navigateToErrorScreen(Widget errorScreen) async {
+    if (mounted) {
+      // Check if the widget is still in the tree
       await Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => InternetConnectionOffNotify()),
+        MaterialPageRoute(builder: (context) => errorScreen),
       );
-      if (defaultTargetPlatform == TargetPlatform.android) {
-        webViewController?.reload();
-      } else if (defaultTargetPlatform == TargetPlatform.iOS ||
-          defaultTargetPlatform == TargetPlatform.macOS) {
-        webViewController?.loadUrl(
-          urlRequest: URLRequest(url: await webViewController?.getUrl()),
-        );
-      }
-    } else {
-      // Internet available, but URL might not be cached
-      await Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => PageNotAvailableScreen()),
-      );
-      if (defaultTargetPlatform == TargetPlatform.android) {
-        webViewController?.reload();
-      } else if (defaultTargetPlatform == TargetPlatform.iOS ||
-          defaultTargetPlatform == TargetPlatform.macOS) {
-        webViewController?.loadUrl(
-          urlRequest: URLRequest(url: await webViewController?.getUrl()),
+    }
+  }
+
+  Future<void> _attemptReload() async {
+    if (_webViewController == null) return;
+
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      await _webViewController?.reload();
+    } else if (defaultTargetPlatform == TargetPlatform.iOS ||
+        defaultTargetPlatform == TargetPlatform.macOS) {
+      final currentUrl = await _webViewController?.getUrl();
+      if (currentUrl != null) {
+        await _webViewController?.loadUrl(
+          urlRequest: URLRequest(url: currentUrl),
         );
       }
     }
   }
 
-  @override
-  void initState() {
-    inAppUpdateAndroid(context);
-
-    super.initState();
-    contextMenu = ContextMenu(
-      menuItems: [
-        ContextMenuItem(
-          id: 1,
-          title: "Special",
-          action: () async {
-            await webViewController?.clearFocus();
-          },
-        ),
-      ],
-      settings: ContextMenuSettings(hideDefaultSystemContextMenuItems: false),
-      onCreateContextMenu: (hitTestResult) async {},
-    );
-
-    pullToRefreshController =
-        kIsWeb ||
-            ![
-              TargetPlatform.iOS,
-              TargetPlatform.android,
-            ].contains(defaultTargetPlatform)
-        ? null
-        : PullToRefreshController(
-            settings: PullToRefreshSettings(color: Colors.blue),
-            onRefresh: () async {
-              if (defaultTargetPlatform == TargetPlatform.android) {
-                webViewController?.reload();
-              } else if (defaultTargetPlatform == TargetPlatform.iOS ||
-                  defaultTargetPlatform == TargetPlatform.macOS) {
-                webViewController?.loadUrl(
-                  urlRequest: URLRequest(
-                    url: await webViewController?.getUrl(),
-                  ),
-                );
-              }
-            },
-          );
-
-    initLastWebUrl();
+  void _handleLoadError() async {
+    bool hasInternet = await _isInternetAvailable();
+    if (!hasInternet) {
+      await _navigateToErrorScreen(InternetConnectionOffNotify());
+    } else {
+      await _navigateToErrorScreen(PageNotAvailableScreen());
+    }
+    // Attempt to reload regardless of the error type after showing the error screen
+    // This allows the user to potentially get back to the webview if the issue was temporary
+    await _attemptReload();
   }
 
   @override
@@ -167,25 +206,23 @@ class WebViewInAppState extends State<WebViewInApp> {
     return PopScope(
       canPop: false,
       onPopInvokedWithResult: (didPop, result) async {
-        bool? canPop = await webViewController?.canGoBack();
-        if (canPop == true) {
-          webViewController?.goBack();
+        if (await _webViewController?.canGoBack() ?? false) {
+          _webViewController?.goBack();
         } else {
           SystemNavigator.pop();
         }
       },
       child: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blue, // Consider making this theme-dependent
         body: SafeArea(
           child: Column(
             children: <Widget>[
               Expanded(
                 child: Stack(
                   children: [
-                    initWidget,
-                    progress < 1.0
-                        ? LinearProgressIndicator(value: progress)
-                        : Container(),
+                    _webViewWidget,
+                    if (_progress < 1.0)
+                      LinearProgressIndicator(value: _progress),
                   ],
                 ),
               ),
@@ -194,26 +231,5 @@ class WebViewInAppState extends State<WebViewInApp> {
         ),
       ),
     );
-  }
-
-  Future<void> deleteFolder(Directory directory) async {
-    if (await directory.exists()) {
-      // List all entities inside the directory (files, subdirectories)
-      final List<FileSystemEntity> entities = await directory.list().toList();
-
-      // Iterate through the list of entities
-      for (FileSystemEntity entity in entities) {
-        if (entity is Directory) {
-          // If the entity is a directory, call deleteFolder recursively
-          await deleteFolder(entity);
-        } else if (entity is File) {
-          // If the entity is a file, delete it
-          await entity.delete();
-        }
-      }
-
-      // After deleting all contents, delete the directory itself
-      await directory.delete();
-    }
   }
 }
